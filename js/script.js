73286d7dc0fc1e704d83e4be8dada0cc2982008a
shchipa <shchipaa@mail.ru>
2017-07-31 22:37:07 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,6 @@
 jQuery(document).ready(function ($) {
-    // $(".kb > span").hover(
-    //     function () {
-    //     	//alert($(this).parents().attr("id"));    //.css({"border":"1px solid red"});
-    //     	//$(this).css({"border":"1px solid red"});
-    //         var id = $(this).parents().attr("id");
-    //         $(".list").filter(function () {
-		// 		if($(this).attr("id") == id){
-		// 			return true;
-		// 		}
-    //         }).slideDown(1000);	//.css({"display":"block"});
-    //     },
-    //     function () {
-    //         $(".list").mouseleave(
-    //         	function () {
-		// 			$(this).slideUp(1000);	//$(this).css({"display":"none"});
-    //             }
-		// 	);
-    //     });
 
-	var kb = $(".kb > span");
-
+var kb = $(".kb > span");
 
 //slideUp/Down left menu kb
     kb.click(function () {
@@ -39,20 +20,6 @@ jQuery(document).ready(function ($) {
 
         $("div.list").not(slide).slideUp(1000);
 	});
-/*
-//display logo kb when hover
-    kb.mouseenter(function () {
-        var id = $(this).parent(".kb").attr("id");
-        //$(this).parents("div").css({"border":"1px solid red"});
-        $(this).parents("div").after('<div class="logo logo_'+ id +'"><img src="pic/small_'+ id +'.gif" alt="'+ id +'" class="logo logo_'+ id +'"/></div>');
-		// $(this).after('<div class="logo logo_'+ id +'"><img src="pic/small_'+ id +'.gif" alt="'+ id +'" class="logo logo_'+ id +'"/></div>');
-    });
-
-    kb.mouseleave(function () {
-        var id = $(this).parent(".kb").attr("id");
-        $("div.logo_"+id).detach();
-    });
-*/
 
 	var content = $("div#content");
 	var header = $("div#header");
